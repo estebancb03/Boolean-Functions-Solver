19f68d41df73d5aa31d819d65975e7ad4e1d17b6
Ajvie <dlizano@outlook.com>
2021-07-08 10:46:13 -0600

    Se crea el metodo AgregarVariable

--- Boolean-Functions-Solver/ListaVariables.cpp
+++ Boolean-Functions-Solver/ListaVariables.cpp
@@ -1,6 +1,16 @@
-#include "listavariables.h"
+#include <iostream>
+#include "ListaVariables.h"
+#include <string.h>
+using namespace std;
 
-ListaVariables::ListaVariables()
-{
-
+void ListaVariables :: agregarVariable(string v, bool vv) {
+    NodoVariables *temp = cabeza;
+    NodoVariables *nuevo = new NodoVariables(v, vv);
+    if(temp == nullptr)
+        cabeza = nuevo;
+    else {
+        while(temp -> getSiguiente() != nullptr)
+            temp = temp -> getSiguiente();
+        temp -> setSiguiente(nuevo);
+    }
 }
