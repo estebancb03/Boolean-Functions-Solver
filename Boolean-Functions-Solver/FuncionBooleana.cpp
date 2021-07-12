52f416eb22207bd05feed1ae10baaa7ed4547586
EstebanUCR <esteban.iglesias@ucr.ac.cr>
2021-07-11 22:55:51 -0600

    Agregar el archivo de buena manera (olvide el guardar) a lña rama principal

---
+++ Boolean-Functions-Solver/FuncionBooleana.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <sstream>
+#include "FuncionBooleana.h"
+using namespace std;
+
+void FuncionBooleana :: seleccionarMinterminos() {
+    string formula;
+    stringstream ssFormula(formulaFuncion);
+    while(getline(ssFormula, formula, '+')) {
+        Mintermino *m = new Mintermino(formula, variableA, variableB, variableC, variableD);
+        listaMinterminos -> agregarMintermino(m);
+    }
+}
+
+void FuncionBooleana :: evaluar() {
+    int i = 0;
+    seleccionarMinterminos();
+    int longitud = getListaMinterminos() -> longitud();
+    bool valoresVerdadMinterminos[longitud];
+    llenarValoresVerdadMinterminos(valoresVerdadMinterminos);
+    while(i < longitud) {
+        if(valoresVerdadMinterminos[i] == true)
+            setValorVerdad(true);
+        i++;
+    }
+}
+
+void FuncionBooleana :: llenarValoresVerdadMinterminos(bool array[]) {
+    int i = 0;
+    string formula;
+    stringstream ssFormula(formulaFuncion);
+    while(getline(ssFormula, formula, '+')) {
+        Mintermino *m =  new Mintermino(formula, variableA, variableB, variableC, variableD);
+        listaMinterminos -> getMinterminoNodo(formula) -> getMintermino() -> setValorVerdad(m -> evaluar());
+        array[i] = listaMinterminos -> getMinterminoNodo(formula) -> getMintermino() -> getValorVerdad();
+        i++;
+    }
+}
