86dc96ae10e99194dfd7aa025e66f761915e173e
EstebanUCR <esteban.iglesias@ucr.ac.cr>
2021-07-11 23:17:43 -0600

    Se modifica la clase Mintermino pero su cpp para que funcione como plantilla

--- Boolean-Functions-Solver/Mintermino.cpp
+++ Boolean-Functions-Solver/Mintermino.cpp
@@ -4,6 +4,26 @@
 #include "Mintermino.h"
 using namespace std;
 
+bool Mintermino :: evaluar() {
+    int cont = 0;
+    string variablesPosibles = "abcd";
+    string letra;
+    bool determinados[variablesPosibles.length()];
+    llenarListaVariables();
+    estandarizar();
+    for(int i = 0; i < variablesPosibles.length(); i++) {
+        letra = variablesPosibles[i];
+        determinados[i] = variables -> getNodo(letra) -> getObj() -> evaluar();
+    }
+    int j = 0;
+    while(j < variables -> longitud()) {
+        if(determinados[j] == false)
+            setValorVerdad(false);
+        j++;
+    }
+    return getValorVerdad();
+}
+
 void Mintermino :: llenarListaVariables() {
     string letra;
     for(int i = 0; i < formulaMintermino.length(); i++) {
@@ -13,24 +33,9 @@ void Mintermino :: llenarListaVariables() {
             letra += formulaMintermino[i + 1];
             i++;
         }
-        else
+        else 
             letra = formulaMintermino[i];
-        if(letra.find("a") != string :: npos) {
-            variables -> agregarVariable(new Variable(letra, introducidoA));
-        }
-        else {
-            if(letra.find("b") != string :: npos) {
-                variables -> agregarVariable(new Variable(letra, introducidoB));
-            }
-            else {
-                if(letra.find("c") != string :: npos) {
-                    variables -> agregarVariable(new Variable(letra, introducidoC));
-                }
-                else if(letra.find("d") != string :: npos) {
-                    variables -> agregarVariable(new Variable(letra, introducidoD));
-                }
-            }
-        }
+        variables -> agregarObjeto(new Variable(letra, introducidos[i]));
     }
 }
 
@@ -52,37 +57,16 @@ void Mintermino :: estandarizar() {
     string faltantes;
     string letra;
     int preLongitud = variables -> longitud();
-    cout << "Formula NO revisada = " << formulaMintermino << endl;
     if(preLongitud != 4) {
         faltantes = averiguaFaltantes();
         for(int i = 0; i < faltantes.length(); i++) {
             letra =  faltantes[i];
-            variables -> agregarVariable(new Variable(letra, true));
+            variables -> agregarObjeto(new Variable(letra, true));
         }
-    }
+    } 
     formulaMintermino = "";
     for(int i = 0; i < variablesPosibles.length(); i++){
         letraRecorrido = variablesPosibles[i];
-        formulaMintermino += variables -> getVariableNodo(letraRecorrido) -> getVariable() -> getLetra();
+        formulaMintermino += variables -> getNodo(letraRecorrido) -> getObj() -> getFormula();
     }
-}
-
-bool Mintermino :: evaluar() {
-    int cont = 0;
-    string variablesPosibles = "abcd";
-    string letra;
-    bool determinados[variablesPosibles.length()];
-    llenarListaVariables();
-    estandarizar();
-    for(int i = 0; i < variablesPosibles.length(); i++) {
-        letra = variablesPosibles[i];
-        determinados[i] = variables -> getVariableNodo(letra) -> getVariable() -> evaluar();
-    }
-    int j = 0;
-    while(j < variables -> longitud()) {
-        if(determinados[j] == false)
-            setValorVerdad(false);;
-        j++;
-    }
-    return getValorVerdad();
 }
