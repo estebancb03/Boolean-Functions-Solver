867537e2f81682c20b3c6b0d99922b997687df2a
EstebanUCR <esteban.iglesias@ucr.ac.cr>
2021-07-11 23:19:17 -0600

    se cambian los parametros del constructor

--- Boolean-Functions-Solver/FuncionBooleana.h
+++ Boolean-Functions-Solver/FuncionBooleana.h
@@ -1,28 +1,27 @@
 #ifndef FUNCIONBOOLEANA_H
 #define FUNCIONBOOLEANA_H
-#include <iostream>
+#include "Lista.h"
 #include "Mintermino.h"
-#include "ListaMinterminos.h"
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
 using namespace std;
 
 class FuncionBooleana {
-    ListaMinterminos *listaMinterminos;
+    Lista<Mintermino> *listaMinterminos;
     string formulaFuncion;
     bool valorVerdad;
-    bool variableA, variableB, variableC, variableD;
+    bool introducidos[4];
     public: 
-        FuncionBooleana(string ff, bool a, bool b, bool c, bool d) {
-            formulaFuncion = ff; listaMinterminos = new ListaMinterminos(); valorVerdad = false;
-            variableA = a; variableB = b; variableC = c, variableD = d;  
-        };
+        FuncionBooleana(string ff, bool intro[]) { formulaFuncion = ff; listaMinterminos = new Lista<Mintermino>(); memcpy(introducidos,intro, 4 * sizeof(int)); valorVerdad = false; };
         ~FuncionBooleana() { delete listaMinterminos; };
         bool getValorVerdad() { return valorVerdad; };
         string getFormula() { return formulaFuncion; };
-        ListaMinterminos *getListaMinterminos() { return listaMinterminos; };
+        Lista<Mintermino> *getListaMinterminos() { return listaMinterminos; };
         void setValorVerdad(bool v) { valorVerdad = v; };
         void seleccionarMinterminos();
         void evaluar();
         void llenarValoresVerdadMinterminos(bool array[]);
 };
 
-#endif 
+#endif
