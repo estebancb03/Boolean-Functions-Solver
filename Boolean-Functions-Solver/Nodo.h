6803a5b2e9e138259ad84a9103537cbfb3ed134c
EstebanUCR <esteban.iglesias@ucr.ac.cr>
2021-07-11 23:01:14 -0600

    se crea nodo de lista emplantillada

---
+++ Boolean-Functions-Solver/Nodo.h
@@ -0,0 +1,16 @@
+#ifndef NODO_H
+#define NODO_H
+template <class T>
+
+class Nodo {
+    T *objeto;
+    Nodo<T> *siguiente;
+    public:
+        Nodo(T *o) { objeto = o; siguiente = nullptr; };
+        ~Nodo() { delete objeto; delete siguiente; };
+        Nodo<T>* getSiguiente() { return siguiente; };
+        T *getObj() { return objeto; };
+        void setSiguiente(Nodo<T> *s) { siguiente = s; };
+};
+
+#endif
