fff58fad13fd02424d7f272983ed9bd867626195
estebancb03 <estebanr.castaneda03@gmail.com>
2021-07-15 01:29:36 -0600

    Se eliminan archivos corruptos

--- Boolean-Functions-Solver/Variable.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Variable.h"
-using namespace std;
-
-bool Variable :: evaluar() {
-    if(formula.find("'") != string :: npos)
-        setValorVerdad(!getValorVerdad());
-    return getValorVerdad();
-}
