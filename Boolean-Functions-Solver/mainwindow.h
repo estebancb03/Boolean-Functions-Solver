9f9bb22275929115dec7087c3d274af187f18e4a
Ajvie <dlizano@outlook.com>
2021-07-21 21:41:58 -0600

    documentacion de la clase

--- Boolean-Functions-Solver/mainwindow.h
+++ Boolean-Functions-Solver/mainwindow.h
@@ -37,39 +37,22 @@ public:
 
 private slots:
     void on_pushButtonBorrar_clicked();
-
     void on_pushButtonEvaluar_clicked();
-
     void on_pushButton00_clicked();
-
     void on_pushButton01_clicked();
-
     void on_pushButton02_clicked();
-
     void on_pushButton03_clicked();
-
     void on_pushButton10_clicked();
-
     void on_pushButton11_clicked();
-
     void on_pushButton12_clicked();
-
     void on_pushButton13_clicked();
-
     void on_pushButton20_clicked();
-
     void on_pushButton21_clicked();
-
     void on_pushButton22_clicked();
-
     void on_pushButton23_clicked();
-
     void on_pushButton30_clicked();
-
     void on_pushButton31_clicked();
-
     void on_pushButton32_clicked();
-
     void on_pushButton33_clicked();
 
 private:
