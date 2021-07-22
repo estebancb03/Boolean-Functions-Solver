9ccd0de368dbc17c88e8ed0f2818f3918f98dedf
estebancb03 <estebanr.castaneda03@gmail.com>
2021-07-22 01:18:31 -0600

    Eliminacion de archivos

--- Boolean-Functions-Solver/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "mainwindow.h"
-#include "Controlador.h"
-#include <QApplication>
-
-int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    Controlador *controlador = new Controlador();
-    return a.exec();
-}
